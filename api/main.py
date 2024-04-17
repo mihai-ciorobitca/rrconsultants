3c4de5d564e95aaec14f8f62ec069781444a511d
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-04-17 11:48:16 +0300

    update

--- api/main.py
+++ api/main.py
@@ -1,38 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Set a secret key for session management
+
+
+# Function to set session language
+@app.route("/set_language/<lang>")
+def set_language(lang):
+    session["language"] = lang
+    return redirect(request.referrer or "/")
+
+
+# Function to get session language
+def get_language():
+    return session.get("language", "ro")  # Default to English if language is not set
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_language = get_language()
+    return render_template(
+        f"/{get_language() }/index.html", current_language=current_language.upper()
+    )
 
 
+# Routes for English pages# Routes for English pages
+@app.route("/consultancy-for-operators")
+@app.route("/consultancy-for-authorities")
+@app.route("/accounting-expertise")
+@app.route("/accounting")
 @app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contability")
-def contability():
-    return render_template("contability.html")
-
-
 @app.route("/cookies")
-def cookies():
-    return render_template("cookies.html")
+def english_routes():
+    session["language"] = "en"
+    return render_template(f"/en/{request.path[1:]}.html")
 
 
-@app.route("/management")
-def management():
-    return render_template("management.html")
+# Routes for Romanian pages
+@app.route("/consultanta-pentru-operatori-economici")
+@app.route("/consultanta-autoritati-contractante")
+@app.route("/expertiza-contabila")
+@app.route("/contabilitate")
+@app.route("/despre-noi")
+@app.route("/cookiuri")
+def romanian_routes():
+    session["language"] = "ro"
+    return render_template(f"/ro/{request.path[1:]}.html")
 
 
-@app.route("/survey")
-def survey():
-    return render_template("survey.html")
-
-
-@app.route("/authority")
-def authority():
-    return render_template("authority.html")
+if __name__ == "__main__":
+    app.run(debug=True)

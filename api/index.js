cd7fcdb3296bac5fe5438699beadfd951d5d107f
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-07-26 10:43:51 +0300

    m

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'static')))
+
 app.get('/', (req, res) => {
-    res.send('RRConsultants');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(port, () => {
